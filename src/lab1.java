3bce9fc403ecea99c1663c47835240286113abfe
Przemko102 <przill000@utp.edu.pl>
2021-03-19 22:08:02 +0100

    nawiasy

--- src/lab1.java
+++ src/lab1.java
@@ -1,6 +1,6 @@
 public class lab1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String jdkVersion = System.getProperty("java.version");
         String jreVersion = System.getProperty("java.runtime.version");
         System.out.println("JDK version: " + jdkVersion);
